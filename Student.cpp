fe4f8be94679c0993d262b4f51fc9ea7ddde666a
Moeen Siddique <moeensiddique092@gmail.com>
2025-12-25 22:15:19 +0500

    Day 2: Added encapsulation and constructor validation

--- Student.cpp
+++ Student.cpp
@@ -1,10 +1,22 @@
 #include<iostream>
 using namespace std;
 class  Student{
-    public:
+    private:
     string name;
     int gradeLeval;
 
+    public:
+    Student(string n, int g){
+        name = n;
+        gradeLeval = g;
+        gradeLeval = 0;
+
+        if (g < 0){
+            cout << "Grade can't be nagetive!" << endl;
+        } else {
+            gradeLeval = g;
+        }
+    }
     void introduceSelf(){
         cout<<"Hi I am "<< name << " I am in grade" <<" "<< gradeLeval << endl;
     }
@@ -12,14 +24,10 @@ class  Student{
 
 int main(){
 
-    Student student1;
-    student1.name = "Ali";
-    student1.gradeLeval = 5;
-    student1.introduceSelf();
+    Student student1("Ali", 5);
+   student1.introduceSelf();
 
-    Student student2;
-    student2.name = "Hassan";
-    student2.gradeLeval = 6;
+    Student student2("Ahmad", 6);
     student2.introduceSelf();
 
     return 0;

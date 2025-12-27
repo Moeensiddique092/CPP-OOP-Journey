2b9a7dd4e0d48d2f5a7854df4b70bea9615c85aa
Moeen Siddique <moeensiddique092@gmail.com>
2025-12-27 16:56:38 +0500

    Day 3: Adding Setter and

--- Student.cpp
+++ Student.cpp
@@ -12,11 +12,25 @@ class  Student{
         gradeLeval = 0;
 
         if (g < 0){
-            cout << "Grade can't be nagetive!" << endl;
+            cout << "Grade can't be negative!" << endl;
         } else {
             gradeLeval = g;
         }
     }
+
+    void Setgrade(int g){
+        if (g < 0){
+            cout<<"Error! Invalid grade."<<endl;
+        } else{
+            gradeLeval = g;
+            cout<< "Grade Updated to "<< g <<endl;
+        }
+    }
+
+    int getGrade(){
+        return gradeLeval;
+    }
+
     void introduceSelf(){
         cout<<"Hi I am "<< name << " I am in grade" <<" "<< gradeLeval << endl;
     }
@@ -25,9 +39,13 @@ class  Student{
 int main(){
 
     Student student1("Ali", 5);
+   student1.Setgrade(8);
+   student1.getGrade();
    student1.introduceSelf();
 
     Student student2("Ahmad", 6);
+    student2.Setgrade(9);
+    student2.getGrade();
     student2.introduceSelf();
 
     return 0;
